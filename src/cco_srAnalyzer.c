c3398b43f2200660648bf3e4dba2743be5be7d1d
Takashi Okumura <taka@faxocr.niph.go.jp>
2012-09-19 23:04:03 +0900

    modified the compression ratio.

--- src/cco_srAnalyzer.c
+++ src/cco_srAnalyzer.c
@@ -199,7 +199,7 @@ int cco_srAnalyzer_writeImage(cco_srAnalyzer *obj, char *file)
 	static int params[] = {CV_IMWRITE_PNG_COMPRESSION, 9, -1};
 #else
 	char conv_str[4096];
-	sprintf(conv_str, "convert %s -quality 9 %s", file, file);
+	sprintf(conv_str, "convert %s -quality 80 %s", file, file);
 #endif
 
 
